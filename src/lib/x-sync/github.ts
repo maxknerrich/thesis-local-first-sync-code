6f3646696a1389c6cdb9e2696bc172cd15ef2b42
Max Knerrich <max.knerrich@outlook.com>
2025-05-20 17:08:01 +0200

    added Github API wrapper

---
+++ src/lib/x-sync/github.ts
@@ -0,0 +1,109 @@
+import { PUBLIC_GITHUB_TOKEN } from '$env/static/public';
+import type { paths } from '$lib/githubapi';
+import { type TDateISO, validateISO861Date } from '$lib/types/date';
+import { paginatedFetch, safeFetch } from '$lib/utils';
+
+const BASIC_HEADERS = {
+	Accept: 'application/vnd.github.text+json',
+	Authorization: `Bearer ${PUBLIC_GITHUB_TOKEN}`,
+	'X-GitHub-Api-Version': '2022-11-28',
+};
+
+export const GH_API = {
+	query: {
+		get_all_repos: async ({ user }: { user: string }) => {
+			const { data, error } = await paginatedFetch<
+				paths['/users/{username}/repos']['get']['responses']['200']['content']['application/json']
+			>(`https://api.github.com/users/${user}/repos`, {
+				headers: BASIC_HEADERS,
+			});
+			if (error) {
+				return { data: null, error };
+			}
+			return { data, error: null };
+		},
+		get_issues: async ({
+			user,
+			repo,
+			since = null,
+		}: {
+			user: string;
+			repo: string;
+			since?: TDateISO | null;
+		}) => {
+			if (since && !validateISO861Date(since)) {
+				return { data: null, error: 'Invalid date format' };
+			}
+			const { data, error } = await paginatedFetch<
+				paths['/repos/{owner}/{repo}/issues']['get']['responses']['200']['content']['application/json']
+			>(`https://api.github.com/repos/${user}/${repo}/issues`, {
+				headers: since ? { ...BASIC_HEADERS, since } : BASIC_HEADERS,
+			});
+			if (error) {
+				return { data: null, error };
+			}
+			return { data, error: null };
+		},
+	},
+	push: {
+		issue: {
+			create: async ({
+				title,
+				body,
+				repo,
+				owner,
+			}: {
+				title: string;
+				body: string;
+				repo: string;
+				owner: string;
+			}) => {
+				const { data, error } = await safeFetch<
+					paths['/repos/{owner}/{repo}/issues']['post']['responses']['201']['content']['application/json']
+				>(`https://api.github.com/repos/${owner}/${repo}/issues`, {
+					method: 'POST',
+					headers: BASIC_HEADERS,
+					body: JSON.stringify({
+						title,
+						body,
+					}),
+				});
+				if (error) {
+					return { data: null, error };
+				}
+				return { data, error: null };
+			},
+			update: async ({
+				issue_number,
+				repo,
+				owner,
+				title,
+				body,
+			}: {
+				issue_number: number;
+				repo: string;
+				owner: string;
+				title?: string;
+				body?: string;
+			}) => {
+				const { data, error } = await safeFetch<
+					paths['/repos/{owner}/{repo}/issues/{issue_number}']['patch']['responses']['200']['content']['application/json']
+				>(
+					`https://api.github.com/repos/${owner}/${repo}/issues/${issue_number}`,
+					{
+						method: 'PATCH',
+						headers: BASIC_HEADERS,
+						body: JSON.stringify({
+							title,
+							body,
+						}),
+					},
+				);
+				if (error) {
+					return { data: null, error };
+				}
+				return { data, error: null };
+			},
+		},
+	},
+};
