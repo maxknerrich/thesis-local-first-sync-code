2ff80552533c713ea7001f620ec9559f986343ee
Max Knerrich <max.knerrich@outlook.com>
2025-05-21 20:06:26 +0200

    added synced store field

--- src/lib/x-sync/db.ts
+++ src/lib/x-sync/db.ts
@@ -3,38 +3,71 @@
  * @author: Max Knerrich
  * @description: This module extends the Dexie database to include a _writeLog table for logging changes made to other tables.
  */
+
 import { Dexie, type EntityTable } from 'dexie';
 import type { WriteLogEntry } from './entry';
+
+interface SyncFields {
+	remote_id: number;
+	remote_created_at: Date;
+	remote_updated_at: Date;
+}
+
+export type SyncStore<T, id extends keyof T> = EntityTable<T & SyncFields, id>;
 
 // Extend the Dexie database interface to include _writeLog table
 declare module 'dexie' {
 	interface Dexie {
 		_writeLog: EntityTable<WriteLogEntry, 'number'>;
+		syncedStores: string[];
+	}
+	interface Version {
+		stores<T extends { [key: string]: string }>(
+			this: Version,
+			stores: T,
+			syncConfig?: { sync: Array<keyof T> },
+		): Version;
 	}
 }
+
 // Add the remote_id index to synced tables
 export function make_sync_store(indexes: string) {
 	return `${indexes}, remote_id`;
 }
-
 export function X_Sync_Addon_Dexie(db: Dexie) {
+	db.syncedStores = [];
 	/**
 	 * Override the Dexie.version([version]).store method to add the _writeLog table.
 	 */
 	db.Version.prototype.stores = Dexie.override(
 		db.Version.prototype.stores,
 		(originalStores) =>
-			function (this: unknown, stores: { [key: string]: string }) {
+			function <T extends { [key: string]: string }>(
+				this: unknown,
+				stores: T,
+				syncConfig?: { sync: Array<keyof T> },
+			) {
+				console.log('this', this);
+				console.log('stores', stores);
+				console.log('syncConfig', syncConfig);
 				// skip if no stores are defined or if no store is a synced store
-				if (
-					!stores ||
-					!Object.values(stores).filter((store) => store.includes('remote_id'))
-						.length
-				) {
+				if (!stores || !syncConfig || syncConfig.sync.length === 0) {
 					return originalStores.call(this, stores);
 				}
+				syncConfig.sync.forEach((store) => {
+					// check if store is already in db.syncedStores
+					if (db.syncedStores.includes(store.toString())) {
+						return;
+					}
+					// check if store is already in stores
+					if (stores[store]) {
+						db.syncedStores.push(store.toString());
+						stores[store] = make_sync_store(stores[store]) as T[keyof T];
+					}
+				});
 				// Add the _writeLog table to the stores object or override it if it exists
-				stores._writeLog = '++number, [object_id+table], operation';
+				(stores as Record<string, string>)['_writeLog'] =
+					'++number, [object_id+table], operation';
 				// Call the original stores function with the modified schema
 				return originalStores.call(this, stores);
 			},
@@ -52,10 +85,8 @@ export function X_Sync_Addon_Dexie(db: Dexie) {
 					return original.apply(this, args);
 				}
 				// check if table has index remote_id and if not, call the original method
-				const hasRemoteId = this.schema.indexes.some(
-					(index) => index.name === 'remote_id',
-				);
-				if (!hasRemoteId) {
+				const isSyncedStore = db.syncedStores.includes(this.name);
+				if (!isSyncedStore) {
 					return original.apply(this, args);
 				}
 				const result = db.transaction(
@@ -88,6 +119,10 @@ export function X_Sync_Addon_Dexie(db: Dexie) {
 				if (this.name === '_writeLog') {
 					return original.apply(this, args);
 				}
+				const isSyncedStore = db.syncedStores.includes(this.name);
+				if (!isSyncedStore) {
+					return original.apply(this, args);
+				}
 				const result = db.transaction(
 					'rw',
 					['_writeLog', this.name],
@@ -116,6 +151,10 @@ export function X_Sync_Addon_Dexie(db: Dexie) {
 			async function (this: Dexie.Table, ...args: unknown[]) {
 				// If the table is _writeLog, call the original method
 				if (this.name === '_writeLog') {
+					return original.apply(this, args);
+				}
+				const isSyncedStore = db.syncedStores.includes(this.name);
+				if (!isSyncedStore) {
 					return original.apply(this, args);
 				}
 				const result = db.transaction(
