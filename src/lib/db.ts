cfd0772d96d6556b41e33cb2e72a471044b52732
Max Knerrich <max.knerrich@outlook.com>
2025-06-04 15:13:28 +0200

    simplified schema

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,22 +1,18 @@
 import { X_Sync_Addon_Dexie } from '$lib/x-sync';
 import { Dexie, type EntityTable } from 'dexie';
-import type { Comment, Issue, Project, User } from './schema';
+import type { Issue, Project } from './schema';
 
 const db = new Dexie('LocalIssueDB', {
 	addons: [X_Sync_Addon_Dexie],
 }) as Dexie & {
 	issues: EntityTable<Issue, 'id'>;
 	projects: EntityTable<Project, 'id'>;
-	comments: EntityTable<Comment, 'id'>;
-	users: EntityTable<User, 'id'>;
 };
 
 db.version(1).stores(
 	{
 		issues: '++id, github_number, user',
 		projects: '++id, user, name',
-		comments: '++id, issue_id, github_id, user',
-		users: '++id, name, login',
 	},
 	{
 		sync: ['issues', 'projects'],
--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,64 +1,16 @@
-import { type } from 'arktype';
+export interface Issue {
+	id: number;
+	title: string;
+	description: string;
+	status: 0 | 1 | 2 | 3;
+	priority: 0 | 1 | 2;
+	github_number?: number;
+	// project_id: number;
+}
 
-export const User = type({
-	'id?': 'number.integer',
-	name: 'string',
-	login: 'string',
-	avatar_url: 'string.url',
-});
-
-export const Status = type('0|1|2|3');
-
-export const StatusLabels: Record<number, string> = {
-	0: 'Todo',
-	1: 'In Progress',
-	2: 'Done',
-	3: 'Backlog',
-};
-
-export const Priority = type('0|1|2');
-export const PriorityLabels: Record<number, string> = {
-	0: 'Low',
-	1: 'Medium',
-	2: 'High',
-};
-
-export const Issue = type({
-	'id?': 'number.integer',
-	title: 'string',
-	'description?': 'string',
-	status: Status,
-	priority: Priority,
-	github_number: 'number.integer',
-	// user: User,
-	'assignee?': type(User, '[]'),
-	created_at: 'Date',
-	updated_at: 'Date',
-	project_id: 'number.integer',
-});
-
-export const Project = type({
-	'id?': 'number.integer',
-	name: 'string',
-	full_name: 'string',
-	created_at: 'Date',
-	updated_at: 'Date',
-	owner: User,
-});
-
-export const Comment = type({
-	'id?': 'number.integer',
-	issue_id: 'number.integer',
-	github_id: 'number.integer',
-	body: 'string',
-	created_at: 'Date',
-	updated_at: 'Date',
-	user: User,
-});
-
-export type Status = typeof Status.infer;
-export type Priority = typeof Priority.infer;
-export type Issue = typeof Issue.infer;
-export type User = typeof User.infer;
-export type Project = typeof Project.infer;
-export type Comment = typeof Comment.infer;
+export interface Project {
+	id: number;
+	name: string;
+	description: string;
+	owner_id: number;
+}
