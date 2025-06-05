c7606cbc2227803e34cadb4781f77a7220983b27
Max Knerrich <max.knerrich@outlook.com>
2025-06-05 16:36:51 +0200

    refactor sync to package

--- src/lib/x-sync/api/github.ts
+++ src/lib/x-sync/api/github.ts
@@ -1,10 +1,15 @@
 import { PUBLIC_GITHUB_TOKEN } from '$env/static/public';
+import { paginatedFetch, safeFetch } from '$lib/utils';
 import { SyncBase } from '$lib/x-sync/sync';
-import type { db } from './db';
+import type Dexie from 'dexie';
+import type {
+	DBObject,
+	PullItem,
+	PullResult,
+	Result,
+	syncConfig,
+} from '../types';
 import type { paths } from './githubapi';
-import type { Issue } from './schema';
-import { paginatedFetch, safeFetch } from './utils';
-import type { DBObject, PullItem, PullResult, Result } from './x-sync/types';
 
 const BASIC_HEADERS = {
 	Accept: 'application/vnd.github.text+json',
@@ -12,7 +17,57 @@ const BASIC_HEADERS = {
 	'X-GitHub-Api-Version': '2022-11-28',
 };
 
-export class GitHubSync extends SyncBase<typeof db> {
+type RemoteIssue =
+	paths['/repos/{owner}/{repo}/issues']['get']['responses']['200']['content']['application/json'][number];
+type RemoteRepo =
+	paths['/users/{username}/repos']['get']['responses']['200']['content']['application/json'][number];
+
+export type GitHubSchemaDefinition = {
+	issues: { github_number: number } & DBObject;
+	repos: DBObject;
+};
+
+export type SchemaConfig<
+	TSchema extends GitHubSchemaDefinition,
+	TDB extends Dexie,
+> = {
+	[K in keyof TSchema]: {
+		tableName: keyof TDB | K;
+		toLocal: (
+			remote: K extends 'issues' ? RemoteIssue : RemoteRepo,
+		) => TSchema[K];
+		toRemote: (
+			local: TSchema[K],
+		) => K extends 'issues' ? Partial<RemoteIssue> : Partial<RemoteRepo>;
+	} & (K extends 'issues'
+		? {
+				repos_to_fetch: () => Promise<{ full_name: string; id: number }[]>;
+				getRepo: (issue: TSchema['issues']) => Promise<string>;
+			}
+		: Record<string, never>);
+};
+
+export class GitHubSync<
+	TSchema extends GitHubSchemaDefinition,
+	TDB extends Dexie,
+> extends SyncBase<TDB> {
+	private token: string;
+	private schema: SchemaConfig<TSchema, Dexie>;
+	constructor({
+		db,
+		token,
+		syncConfig,
+		schema,
+	}: {
+		db: TDB;
+		token: string;
+		syncConfig?: syncConfig;
+		schema: SchemaConfig<TSchema, Dexie>;
+	}) {
+		super({ db, ...(syncConfig && { syncConfig }) });
+		this.schema = schema;
+		this.token = token;
+	}
 	async pullData({
 		table,
 		since,
@@ -21,47 +76,38 @@ export class GitHubSync extends SyncBase<typeof db> {
 		since?: Date;
 	}): Promise<Result<PullResult>> {
 		switch (table) {
-			case 'issues': {
-				const projects = await this.db.projects
-					.filter((project) => project.active === true)
-					.toArray();
+			case this.schema.issues.tableName: {
+				const projects = await this.schema.issues.repos_to_fetch();
 				const issues = await Promise.all(
 					projects.map((project) =>
 						this.get_issues({
 							fullname: project.full_name,
 							since: since,
 						}).then((res) => {
-							return res.map((issue) => ({
-								remote_id: issue.id.toString(),
-								github_number: issue.number,
-								title: issue.title,
-								description: issue.body,
-								status: issue.state === 'open' ? 0 : 2,
-								project_id: project.id,
-							}));
+							return res.map((issue: RemoteIssue) =>
+								this.schema.issues.toLocal(issue),
+							);
 						}),
 					),
 				).then((results) => results.flat());
-				return { error: null, data: issues };
+				return { error: null, data: issues as PullItem[] };
 			}
-			case 'projects': {
+			case this.schema.repos.tableName: {
 				const data = await paginatedFetch<
 					paths['/users/{username}/repos']['get']['responses']['200']['content']['application/json']
 				>(`https://api.github.com/user/repos`, {
 					headers: BASIC_HEADERS,
 				}).then((res) => {
-					return res.data?.map((repo) => ({
-						remote_id: repo.id.toString(),
-						full_name: repo.full_name,
-						name: repo.name,
-						description: repo.description || '',
-						active: false,
-					}));
+					return (
+						res.data?.map((repo: RemoteRepo) =>
+							this.schema.repos.toLocal(repo),
+						) || []
+					);
 				});
 				if (!data) {
 					throw new Error('Failed to fetch projects from GitHub');
 				}
-				return { data, error: null };
+				return { data: data as PullItem[], error: null };
 			}
 			default:
 				return { data: null, error: new Error(`Unknown table: ${table}`) };
@@ -78,17 +124,17 @@ export class GitHubSync extends SyncBase<typeof db> {
 		data: Omit<DBObject, 'remote_id' | 'id'>;
 	}): Promise<{ key: number; changes: Partial<PullItem> }> {
 		switch (table) {
-			case 'issues': {
-				const project = await this.db.projects.get(
-					(item as unknown as Issue).project_id,
+			case this.schema.issues.tableName: {
+				const project = await this.schema.issues.getRepo(
+					item as TSchema['issues'],
 				);
 				const { data: pullData, error } = await safeFetch<
 					paths['/repos/{owner}/{repo}/issues']['post']['responses']['201']['content']['application/json']
-				>(`https://api.github.com/repos/${project?.full_name}/issues`, {
+				>(`https://api.github.com/repos/${project}/issues`, {
 					method: 'POST',
 					headers: BASIC_HEADERS,
 					body: JSON.stringify(
-						this.mapLocalToRemoteIssue(data as unknown as Issue),
+						this.schema.issues.toRemote(data as TSchema['issues']),
 					),
 				});
 				if (error) {
@@ -117,26 +163,26 @@ export class GitHubSync extends SyncBase<typeof db> {
 		item: DBObject;
 	}): Promise<void> {
 		switch (table) {
-			case 'issues': {
-				const remoteData = this.mapLocalToRemoteIssue(data as unknown as Issue);
+			case this.schema.issues.tableName: {
+				const remoteData = this.schema.issues.toRemote(
+					data as TSchema['issues'],
+				);
 				if (!remoteData) {
 					//nothing to update
 					return;
 				}
-				const project = await this.db.projects.get(
-					(item as unknown as Issue).project_id,
+				const project = await this.schema.issues.getRepo(
+					item as TSchema['issues'],
 				);
+				const number = item.github_number;
 
 				const { error } = await safeFetch<
 					paths['/repos/{owner}/{repo}/issues']['post']['responses']['201']['content']['application/json']
-				>(
-					`https://api.github.com/repos/${project?.full_name}/issues/${(item as unknown as Issue).github_number}`,
-					{
-						method: 'PATCH',
-						headers: BASIC_HEADERS,
-						body: JSON.stringify(remoteData),
-					},
-				);
+				>(`https://api.github.com/repos/${project}/issues/${number}`, {
+					method: 'PATCH',
+					headers: BASIC_HEADERS,
+					body: JSON.stringify(remoteData),
+				});
 				if (error) {
 					throw new Error(`Failed to create issue: ${error}`);
 				}
@@ -167,37 +213,18 @@ export class GitHubSync extends SyncBase<typeof db> {
 		if (since && !(since instanceof Date)) {
 			throw new Error('Since must be a Date object or null');
 		}
+		console.log(since, since?.toISOString());
+		const queryParams = since
+			? `?since=${since.toISOString()}&state=all`
+			: '?state=all';
 		const { data, error } = await paginatedFetch<
 			paths['/repos/{owner}/{repo}/issues']['get']['responses']['200']['content']['application/json']
-		>(
-			`https://api.github.com/repos/${fullname}/issues${since ? `?since=${since.toISOString()}` : ''}`,
-			{
-				headers: BASIC_HEADERS,
-			},
-		);
+		>(`https://api.github.com/repos/${fullname}/issues${queryParams}`, {
+			headers: BASIC_HEADERS,
+		});
 		if (error) {
 			throw new Error(`Failed to fetch issues: ${error}`);
 		}
 		return data;
-	}
-
-	private mapLocalToRemoteIssue(
-		issue: Issue,
-	): Partial<
-		paths['/repos/{owner}/{repo}/issues']['post']['responses']['201']['content']['application/json']
-	> {
-		const remoteDelta: Partial<
-			paths['/repos/{owner}/{repo}/issues']['post']['responses']['201']['content']['application/json']
-		> = {};
-		if (issue.title) {
-			remoteDelta.title = issue.title;
-		}
-		if (issue.description) {
-			remoteDelta.body = issue.description;
-		}
-		if (issue.status) {
-			remoteDelta.state = issue.status === 2 ? 'closed' : 'open';
-		}
-		return remoteDelta;
 	}
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,17 @@
 	import { PUBLIC_GITHUB_TOKEN } from "$env/static/public";
 	import { db } from "$lib/db";
 	import { GH_API } from "$lib/github";
-	import { GitHubSync } from "$lib/sync";
+	import type { Issue, Project } from "$lib/schema";
+	import { GitHubSync } from "$lib/x-sync";
 
-	const sync = new GitHubSync({
+	type Schema = {
+		issues: Required<Issue>;
+		repos: Required<Project>;
+	};
+
+	const sync = new GitHubSync<Schema, typeof db>({
 		db,
+		token: PUBLIC_GITHUB_TOKEN,
 		syncConfig: {
 			issues: {
 				mode: "auto",
@@ -16,20 +23,98 @@
 				path: "r",
 			},
 		},
+		schema: {
+			issues: {
+				tableName: "issues",
+				repos_to_fetch: () =>
+					db.projects
+						.filter((project) => project.active === true)
+						.toArray()
+						.then((result) =>
+							result.map((project) => ({
+								full_name: project.full_name,
+								id: project.id,
+							})),
+						),
+				getRepo: (issue) => issue.project_id,
+				toLocal: (remote) => ({
+					title: remote.title,
+					description: remote.body,
+					status: remote.state === "open" ? 0 : 2,
+					github_number: remote.number,
+					remote_id: remote.id,
+				}),
+				toRemote: (
+					local,
+				): Partial<{ title: string; body: string; state: string }> => {
+					const remoteDelta: Partial<{
+						title: string;
+						body: string;
+						state: string;
+					}> = {};
+					if (local.title) {
+						remoteDelta.title = local.title;
+					}
+					if (local.description) {
+						remoteDelta.body = local.description;
+					}
+					if (local.status !== undefined) {
+						remoteDelta.state = local.status === 2 ? "closed" : "open";
+					}
+					return remoteDelta;
+				},
+			},
+			repos: {
+				tableName: "projects",
+				toLocal: (remote) => ({
+					name: remote.name,
+					description: remote.description,
+					remote_id: remote.id,
+					full_name: remote.full_name,
+				}),
+			},
+		},
 	});
 
 	async function getRepos() {
 		// Get the list of repositories
 		const repos = await GH_API.query.get_all_repos();
 	}
+	fetch(
+		"https://api.github.com/repos/maxknerrich/BachelorTestProject/issues?since=2023-05-01T00:00:00Z&state=all",
+		{
+			headers: {
+				Accept: "application/vnd.github.text+json",
+				Authorization: `Bearer ${PUBLIC_GITHUB_TOKEN}`,
+				"X-GitHub-Api-Version": "2022-11-28",
+			},
+		},
+	)
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("Network response was not ok");
+			}
+			return response.json();
+		})
+		.then((data) => {
+			console.log(data);
+		})
+		.catch((error) => {
+			console.error(
+				"There has been a problem with your fetch operation:",
+				error,
+			);
+		});
 
 	async function deleteDB() {
 		await db.delete();
 		localStorage.clear();
 	}
 
-	async function Sync() {
+	async function SyncIssue() {
 		sync.syncTable("issues");
+	}
+	async function SyncProject() {
 		sync.syncTable("projects");
 	}
 
@@ -41,6 +126,15 @@
 			status: 1,
 			priority: 1,
 			project_id: 1,
+		});
+	}
+	async function createProject() {
+		const project = await db.projects
+			.where("name")
+			.equals("BachelorTestProject")
+			.first();
+		await db.projects.update(project?.id, {
+			active: true,
 		});
 	}
 	async function updateIssue() {
@@ -63,7 +157,9 @@
 </script>
 
 <button onclick={addIssue}>Add issue</button>
+<button onclick={createProject}>createProject </button>
 <button onclick={updateIssue}>Update issues</button>
 <button onclick={getRepos}>Get Repos</button>
 <button onclick={deleteDB}>Delete DB</button>
-<button onclick={Sync}>Sync</button>
+<button onclick={SyncIssue}>Sync Issues</button>
+<button onclick={SyncProject}>Sync Projects</button>
